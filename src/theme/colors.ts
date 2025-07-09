21e0e52de0ec9bbdf0123744d8eb24187c13e85a
daniel <ngumodaniel80@gmail.com>
2025-07-10 00:46:51 +0300

    first commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+// src/theme/colors.ts
+
+const colors = {
+  primary: '#e01c4e',    // Red-ish (Trust & Action)
+  secondary: '#15479e',  // Deep Blue (Security & Stability)
+  accent: '#17b5a7',     // Teal (Progress & Clarity)
+};
+
+export default colors;
